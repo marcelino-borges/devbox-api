fae7bc93a9fffac7d7817fc94ef8663fe77ea4ba
Marcelino Borges <botelho_gt@hotmail.com>
2021-08-12 02:32:19 -0300

    cors update

--- src/server.ts
+++ src/server.ts
@@ -32,12 +32,6 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true}))
 app.use(express.json());
 
-// app.use(function(req, res, next) {
-//     res.header("Access-Control-Allow-Origin", "devbox.eng.br"); // update to match the domain you will make the request from
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//     next();
-//   });
-
 app.use("/api/v1/portfolio", portfolioRouter);
 app.use("/api/v1/team", teamRouter);
 
