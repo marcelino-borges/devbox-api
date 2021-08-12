ea35d4503737be0234f282298b12900b4d688b46
Marcelino Borges <botelho_gt@hotmail.com>
2021-08-12 02:26:31 -0300

    cors rollback

--- src/routes/portfolio-routes.ts
+++ src/routes/portfolio-routes.ts
@@ -4,26 +4,8 @@ import * as portfolioController from "./../controllers/portfolio-controller";
 
 const portfolioRouter = express.Router();
 
-var corsWhitelist = [
-    'http://devbox.eng.br',
-    'https://devbox.eng.br'
-];
-
-var corsOptions = {
-    origin: function (origin: any, callback: any) {
-        if (corsWhitelist.indexOf(origin) !== -1) {
-            console.log("origin known");
-            callback(null, true);
-        } else {
-            console.log("origin not known");
-            callback(new Error('Not allowed by CORS'));
-        }
-      },
-    optionsSuccessStatus: 200, // legacy browser support
-}
-
-portfolioRouter.get("/", cors(), portfolioController.getAllJobs);
-portfolioRouter.get("/id/:id", cors(corsOptions), portfolioController.getJobById);
-portfolioRouter.get("/name/:name", cors(corsOptions), portfolioController.getJobByName);
+portfolioRouter.get("/", portfolioController.getAllJobs);
+portfolioRouter.get("/id/:id", portfolioController.getJobById);
+portfolioRouter.get("/name/:name", portfolioController.getJobByName);
 
 export default portfolioRouter;
--- src/server.ts
+++ src/server.ts
@@ -1,29 +1,15 @@
 import dotenv from "dotenv";
+import cors from "cors";
 import helmet from 'helmet';
 import express from "express";
-import cors from "cors";
 import portfolioRouter from "./routes/portfolio-routes";
 import teamRouter from "./routes/team-routes";
-import * as portfolioController from "./controllers/portfolio-controller";
 
 dotenv.config();
 let PORT = parseInt(process.env.PORT as string, 10);
 console.log("PORT: ", PORT);
 
 const app = express();
-
-app.use(helmet());
-app.use(express.json())
-app.use(express.urlencoded({ extended: true}))
-app.use(express.json());
-
-// app.use(function(req, res, next) {
-//     res.header("Access-Control-Allow-Origin", "devbox.eng.br"); // update to match the domain you will make the request from
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//     next();
-// });
-
-//app.use("/api/v1/portfolio", portfolioRouter);
 
 var corsWhitelist = [
     'http://devbox.eng.br',
@@ -33,17 +19,26 @@ var corsWhitelist = [
 var corsOptions = {
     origin: function (origin: any, callback: any) {
         if (corsWhitelist.indexOf(origin) !== -1) {
-            console.log("origin known");
-            callback(null, true);
+          callback(null, true)
         } else {
-            console.log("origin not known");
-            callback(new Error('Not allowed by CORS'));
+          callback(new Error('Not allowed by CORS'))
         }
       },
     optionsSuccessStatus: 200, // legacy browser support
 }
-app.use(cors());
-app.get("/api/v1/portfolio", portfolioController.getAllJobs);
+app.use(cors(corsOptions));
+app.use(helmet());
+app.use(express.json())
+app.use(express.urlencoded({ extended: true}))
+app.use(express.json());
+
+// app.use(function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "devbox.eng.br"); // update to match the domain you will make the request from
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+//   });
+
+app.use("/api/v1/portfolio", portfolioRouter);
 app.use("/api/v1/team", teamRouter);
 
 console.log("PORT being used: ", PORT);
