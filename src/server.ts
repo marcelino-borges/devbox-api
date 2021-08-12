170180db20aaf911fb919e3c9b65ea45e279be79
Marcelino Borges <botelho_gt@hotmail.com>
2021-08-12 01:28:59 -0300

    Cors white list

--- src/server.ts
+++ src/server.ts
@@ -11,28 +11,33 @@ console.log("PORT: ", PORT);
 
 const app = express();
 
-// var corsWhitelist = [
-//     'http://localhost:3000',
-//     'http://devbox.eng.br',
-//     'https://devbox.eng.br'
-// ];
+var corsWhitelist = [
+    'http://devbox.eng.br',
+    'https://devbox.eng.br'
+];
 
-// var corsOptions = {
-//     origin: function (origin: any, callback: any) {
-//         if (corsWhitelist.indexOf(origin) !== -1) {
-//           callback(null, true)
-//         } else {
-//           callback(new Error('Not allowed by CORS'))
-//         }
-//       },
-//     optionsSuccessStatus: 200, // legacy browser support
-//     //methods: "GET, PUT, POST"
-// }
-app.use(cors());
+var corsOptions = {
+    origin: function (origin: any, callback: any) {
+        if (corsWhitelist.indexOf(origin) !== -1) {
+          callback(null, true)
+        } else {
+          callback(new Error('Not allowed by CORS'))
+        }
+      },
+    optionsSuccessStatus: 200, // legacy browser support
+}
+
+app.use(cors(corsOptions));
 app.use(helmet());
 app.use(express.json())
 app.use(express.urlencoded({ extended: true}))
 app.use(express.json());
+
+// app.use(function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "devbox.eng.br"); // update to match the domain you will make the request from
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+//   });
 
 app.use("/api/v1/portfolio", portfolioRouter);
 app.use("/api/v1/team", teamRouter);
